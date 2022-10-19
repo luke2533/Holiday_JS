ee496eca1bc0d6151da5b109e0e5b36aefc63670
lukef <lukefoxwell1@gmail.com>
2022-10-19 10:55:04 +0100

    Removing nested loop

--- holiday.js
+++ holiday.js
@@ -3,15 +3,15 @@ let msgTwo = ["2 Nights","3 Nights", "4 Nights", "5 Nights","6 Nights", "1 Week"
 let msgThree = ["Plane", "Ferry", "Cruise", "Train"];
 
 function randomMsg(arr) {
-    let message = "";
     for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr.length; j++) {
-            for (let k = 0; k < arr.length; k++) {
-                console.log(arr[i][j][k]);
+        for (let j = 0; j < arr[j].length; j++) {
+            for (let k = 0; k < arr[k].length; k++) {
             }
         }
     }
-    return message;
 }
 
-randomMsg(msgOne, msgTwo. msgThree);
+console.log("Find your holiday today!");
+randomMsg(msgOne, msgTwo, msgThree);
+let message = "Your destination " + randomMsg(msgOne) + " for " + randomMsg(msgTwo) + " traveling by " + randomMsg(msgThree);
+console.log(message);
