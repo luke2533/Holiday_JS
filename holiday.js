e310c1c869da95b984b467e504377850548c17c1
lukef <lukefoxwell1@gmail.com>
2022-10-19 11:11:36 +0100

    Random message complete

--- holiday.js
+++ holiday.js
@@ -1,17 +1,10 @@
-let msgOne = ["Paris", "Brussels", "Amsterdam", "Berlin", "Copenhagen", "Prague", "Vienna", "Budapest", "Rome", "Zurich", "Madrid", "Lisbon"];
-let msgTwo = ["2 Nights","3 Nights", "4 Nights", "5 Nights","6 Nights", "1 Week", "2 Weeks"];
-let msgThree = ["Plane", "Ferry", "Cruise", "Train"];
+let arrOne = ["Paris", "Brussels", "Amsterdam", "Berlin", "Copenhagen", "Prague", "Vienna", "Budapest", "Rome", "Zurich", "Madrid", "Lisbon"]; // 12
+let arrTwo = ["2 Nights","3 Nights", "4 Nights", "5 Nights","6 Nights", "1 Week", "2 Weeks"]; // 7
+let arrThree = ["Plane", "Ferry", "Cruise", "Train"]; // 4
 
-function randomMsg(arr) {
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr[j].length; j++) {
-            for (let k = 0; k < arr[k].length; k++) {
-            }
-        }
-    }
-}
+msgOne = Math.floor(Math.random() * 13);
+msgTwo = Math.floor(Math.random() * 7);
+msgThree = Math.floor(Math.random() * 4);
 
 console.log("Find your holiday today!");
-randomMsg(msgOne, msgTwo, msgThree);
-let message = "Your destination " + randomMsg(msgOne) + " for " + randomMsg(msgTwo) + " traveling by " + randomMsg(msgThree);
-console.log(message);
+console.log("Your destination will be " + arrOne[msgOne] + " for " + arrTwo[msgTwo] + " traveling by " + arrThree[msgThree] + "!");
